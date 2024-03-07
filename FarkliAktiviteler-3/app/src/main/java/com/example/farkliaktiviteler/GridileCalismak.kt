3dffc59b92d4d26c9d174d183c0401bb600b8ebb
Bilal AKIN <bilalimakin@gmail.com>
2024-03-07 14:50:45 +0300

    Ikıncı Aktivize Grid iLe Izgara Oluşturma

---
+++ FarkliAktiviteler-3/app/src/main/java/com/example/farkliaktiviteler/GridileCalismak.kt
@@ -0,0 +1,11 @@
+package com.example.farkliaktiviteler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GridileCalismak : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gridile_calismak)
+    }
+}
